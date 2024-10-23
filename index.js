174628cb83f6a2ac3ec0e7afa3393f39ee825d4d
francamateus93 <francamateus@gmail.com>
2024-10-23 11:34:30 +0200

    Ejercicio 5 completed

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // Ejercicio 1.1: Arrow functions - Nivel 1
 
-// Ejercicio 1
+/*----------- Ejercicio 1*/
 
 const add = (a, b) => a + b;
 
-// Ejercicio 2
+/*----------- Ejercicio 2*/
+console.log("----- Ej. 2:")
+
 
 const randonNumber = () => Math.floor(Math.random() * 100);
-
 console.log(randonNumber(10));
 
-// Ejercicio 3
+
+/*----------- Ejercicio 3*/
+console.log("----- Ej. 3:")
+
 
 class Person {
   constructor(name) {
@@ -22,12 +26,12 @@ class Person {
 let person = new Person('John Smith');
 person.greet();
 
-// Ejercicio 1.1: Arrow functions - Nivel 2
 
-// Ejercicio 4
+/*----------- Ejercicio 4 - Nivel 2*/
+console.log("----- Ej. 4:")
+
 
 const arr = [1, 3, 5, 7, 9];
-
 const printNumbers = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
@@ -37,3 +41,13 @@ const printNumbers = (arr) => {
 printNumbers(arr);
 
 
+/*----------- Ejercicio 5 - Nivel 3*/
+console.log("----- Ej. 5:")
+
+
+const printAfter = () => {
+  setTimeout( () => {
+    console.log("My Message was delivered.");}, 3000);
+};
+
+printAfter();
