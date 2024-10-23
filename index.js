30a09561d29d452ecb78c37f8c5b8ce2e6570d92
francamateus93 <francamateus@gmail.com>
2024-10-23 11:05:41 +0200

    Ejercicio 4 completed

--- index.js
+++ index.js
@@ -21,3 +21,19 @@ class Person {
 
 let person = new Person('John Smith');
 person.greet();
+
+// Ejercicio 1.1: Arrow functions - Nivel 2
+
+// Ejercicio 4
+
+const arr = [1, 3, 5, 7, 9];
+
+const printNumbers = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printNumbers(arr);
+
+
